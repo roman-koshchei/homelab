619a4b3455f377a0166d2c2bb70b3b1f3d6beec9
Roman Koshchei <romankoshchei@gmail.com>
2024-09-19 17:57:09 +0300

    feedhub

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ SERVER="rk@192.168.31.10"
 scp ./src/configuration.nix $SERVER:/tmp 
 scp ./src/cloud.nix $SERVER:/tmp
 scp ./src/services/feedhub.nix $SERVER:/tmp
+scp ./src/services/pocketbase.nix $SERVER:/tmp
 
 ssh -t $SERVER "
   sudo mv /tmp/cloud.nix /etc/nixos/cloud.nix
   sudo mv /tmp/configuration.nix /etc/nixos/configuration.nix
   sudo mv /tmp/feedhub.nix /etc/nixos/services/feedhub.nix
+  sudo mv /tmp/pocketbase.nix /etc/nixos/services/pocketbase.nix
   sudo nixos-rebuild switch
 "
