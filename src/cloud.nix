b527fb9f42a598672db8012ee9224c2fe31fefc2
Roman Koshchei <romankoshchei@gmail.com>
2024-09-20 18:32:44 +0300

    sync

--- src/cloud.nix
+++ src/cloud.nix
@@ -27,8 +27,18 @@ in {
     # needs to be moved to separate config file, because changes during runtime
     virtualHosts."cloud.cookingweb.dev" = {
       extraConfig = ''
-        respond "Cooking Web self-hosted cloud is in a research stage. It runs on an old laptop with NixOS. At some point it will have UI. Checkout all info on cookingweb.dev or my X."
+        reverse_proxy :3001
       '';
+      # extraConfig = ''
+      #   respond "Cooking Web self-hosted cloud is in a research stage. It runs on an old laptop with NixOS. At some point it will have UI. Checkout all info on cookingweb.dev or my X."
+      # '';
+    };
+  };
+
+  services.uptime-kuma = {
+    enable = true;
+    settings = {
+      PORT = "3001";
     };
   };
 }
